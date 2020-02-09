5d3b8f144b49d787fafa265689de013d76b8ec3c
ms-online <27732357@qq.com>
2020-02-09 14:26:23 +0800

    lesson-1

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // get input element
-let fliterInput = document.getElementById("fliterInput");
+let filterInput = document.getElementById("filterInput");
 
 // add event listener
-fliterInput.addEventListener("keyup", fliterNames);
+filterInput.addEventListener("keyup", filterNames);
 
-function fliterNames() {
+function filterNames() {
   //get value of input
-  let fliterValue = document.getElementById("fliterInput").value.toUpperCase();
+  let filterValue = document.getElementById("filterInput").value.toUpperCase();
 
   //get ul names
   let ul = document.getElementById("names");
@@ -20,7 +20,7 @@ function fliterNames() {
     let a = li[i].getElementsByTagName("a")[0];
 
     //if matched
-    if (a.innerHTML.toUpperCase().indexOf(fliterValue) > -1) {
+    if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
       li[i].style.display = "";
     } else {
       li[i].style.display = "none";
